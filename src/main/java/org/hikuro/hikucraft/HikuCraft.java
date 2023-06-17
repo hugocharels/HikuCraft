fe63604d56cd7c7a2e00b639ac251b92dc925941
hugocharels <hugocharels@hotmail.com>
2023-06-17 14:15:19 +0200

    first commit

---
+++ src/main/java/org/hikuro/hikucraft/HikuCraft.java
@@ -0,0 +1,17 @@
+package org.hikuro.hikucraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HikuCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
