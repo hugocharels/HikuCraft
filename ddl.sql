4115009821e9e435c7029978fae1c9ca74903559
Hugo <93338995+hugocharels@users.noreply.github.com>
2023-06-18 18:05:22 +0200

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,34 +1,34 @@
-DROP DATABASE IF EXISTS "HikuCraft";
-CREATE DATABASE "HikuCraft";
-USE "HikuCraft";
+DROP DATABASE IF EXISTS 'HikuCraft';
+CREATE DATABASE 'HikuCraft';
+USE 'HikuCraft';
 
 
 -- Users
-CREATE USER IF NOT EXISTS "HikuCraft_admin"@"localhost" IDENTIFIED BY "HikuCraft_admin_psw";
-CREATE USER IF NOT EXISTS "HikuCraft_user"@"localhost" IDENTIFIED BY "HikuCraft_user_psw";
+CREATE USER IF NOT EXISTS 'HikuCraft_admin'@'localhost' IDENTIFIED BY 'HikuCraft_admin_psw';
+CREATE USER IF NOT EXISTS 'HikuCraft_user'@'localhost' IDENTIFIED BY 'HikuCraft_user_psw';
 
-GRANT ALL PRIVILEGES ON "HikuCraft".* TO "HikuCraft_admin"@"localhost";
-GRANT SELECT ON "HikuCraft".* TO "HikuCraft_user"@"localhost";
-GRANT INSERT ON "HikuCraft".* TO "HikuCraft_user"@"localhost";
-GRANT UPDATE(TimePlayed) ON "HikuCraft". TO "HikuCraft_user"@"localhost";
-GRANT DELETE ON "HikuCraft"."Home" TO "HikuCraft_user"@"localhost";
+GRANT ALL PRIVILEGES ON 'HikuCraft'.* TO 'HikuCraft_admin'@'localhost';
+GRANT SELECT ON 'HikuCraft'.* TO 'HikuCraft_user'@'localhost';
+GRANT INSERT ON 'HikuCraft'.* TO 'HikuCraft_user'@'localhost';
+GRANT UPDATE(TimePlayed) ON 'HikuCraft'. TO 'HikuCraft_user'@'localhost';
+GRANT DELETE ON 'HikuCraft'.'Home' TO 'HikuCraft_user'@'localhost';
 
 
 -- Tables
 
-CREATE TABLE "PlayerData" (
+CREATE TABLE 'PlayerData' (
 	UUID VARCHAR(36) NOT NULL,
 	TimePlayed TIME(1),
 
 	PRIMARY KEY (UUID),
 	UNIQUE (UUID),
 	
-	CONSTRAINT "PlayerData_TimePlayed" CHECK (TimePlayed >= 0),
+	CONSTRAINT 'PlayerData_TimePlayed' CHECK (TimePlayed >= 0),
 
 );
 
 
-CREATE TABLE "Home" (
+CREATE TABLE 'Home' (
 	UUID VARCHAR(36) NOT NULL,
 	Name VARCHAR(16) NOT NULL,
 	World VARCHAR(16) NOT NULL,
@@ -38,9 +38,9 @@ CREATE TABLE "Home" (
 
 	PRIMARY KEY (UUID, Name),
 	UNIQUE (UUID, Name),
-	FOREIGN KEY (UUID) REFERENCES "PlayerData" (UUID) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (UUID) REFERENCES 'PlayerData' (UUID) ON DELETE CASCADE ON UPDATE CASCADE,
 	
-	CONSTRAINT "Home_World" CHECK (World IN ('world', 'world_nether', 'world_the_end')),
+	CONSTRAINT 'Home_World' CHECK (World IN ('world', 'world_nether', 'world_the_end')),
 
 );
 
